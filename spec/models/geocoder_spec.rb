7e2db0be66c641e361b347ab8b6c5e50a761a31c
Jonathan <jon.dandois@gmail.com>
2016-11-03 14:35:09 -0400

    adding more google geocoder statuses and tests

--- spec/models/geocoder_spec.rb
+++ spec/models/geocoder_spec.rb
@@ -4,9 +4,8 @@ require 'Geocoder'
 testData = { 
   null_request: '',
   good_request: '8 Market Place Baltimore MD 21202',
-  garbage_request: 'asdfaewrves5hw4aergqqv3 \!@%#',
-  blank_request: ' ',
-
+  garbage_request: '\asdfaewrves5hw4aergqqv3 \!@%#',
+  blank_request: ' '
 }
 
 describe Geocoder, type: :model do
@@ -44,14 +43,52 @@ describe Geocoder, type: :model do
   end
 
   context "when given a blank request" do
-    let(:response) {Geocoder.search(testData[:blannk_request])}
-    it "returns error" do
-      expect(400...500).to cover(response.status)
+    let(:response) {Geocoder.search(testData[:blank_request])}
+    it "succeeds" do
+      expect(200...300).to cover(response.status)
     end
-    it "returns INVALID_REQUEST" do
+    it "returns ZERO_RESULTS" do
       bodyJson = JSON.parse(response.body)
-      expect(bodyJson['status']).to eq('INVALID_REQUEST')
+      expect(bodyJson['status']).to eq('ZERO_RESULTS')
     end
   end
 
+  context "when over the quota limit" do
+    let(:response) {Geocoder.search(testData[:good_request])}
+    it "returns error" do
+      skip "skipped because we cant really test this response"
+      expect(400...500).to cover(response.status)
+    end
+    it "returns OVER_QUOTA_LIMIT" do
+      skip "skipped because we cant really test this response"
+      bodyJson = JSON.parse(response.body)
+      expect(bodyJson['status']).to eq('OVER_QUOTA_LIMIT')
+    end
+  end
+
+  context "when request is denied" do
+    let(:response) {Geocoder.search(testData[:good_request])}
+    it "returns error" do
+      skip "skipped because we cant really test this response"
+      expect(400...500).to cover(response.status)
+    end
+    it "returns REQUEST_DENIED" do
+      skip "skipped because we cant really test this response"
+      bodyJson = JSON.parse(response.body)
+      expect(bodyJson['status']).to eq('REQUEST_DENIED')
+    end
+  end
+
+  context "when an unknown error occurs" do
+    let(:response) {Geocoder.search(testData[:good_request])}
+    it "returns error" do
+      skip "skipped because we cant really test this response"
+      expect(400...500).to cover(response.status)
+    end
+    it "returns UNKNOWN_ERROR" do
+      skip "skipped because we cant really test this response"
+      bodyJson = JSON.parse(response.body)
+      expect(bodyJson['status']).to eq('UNKNOWN_ERROR')
+    end
+  end
 end
