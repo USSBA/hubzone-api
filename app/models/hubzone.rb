3e87efa1f2829de20226bae0c556e34157805a88
Jonathan <jon.dandois@gmail.com>
2016-11-15 10:09:55 -0500

    HUB-45 adding in additional responses to hubzone api query

--- app/models/hubzone.rb
+++ app/models/hubzone.rb
@@ -6,9 +6,15 @@ class Hubzone
     geocoder_results = JSON.parse g.body
     case geocoder_results['status']
     when invalid_request[:status]
-      return geocoder_results
+      return invalid_request()
     when zero_results[:status]
-      return geocoder_results
+      return zero_results('No results returned by geocoder')
+    when over_limit[:status]
+      return over_limit()
+    when request_denied[:status]
+      return request_denied()
+    when unknown_error[:status]
+      return unknown_error()
     end
 
     results = geocoder_results['results'][0]
@@ -34,14 +40,30 @@ class Hubzone
 
   private
 
+  def self.zero_results(message = "Zero results")
+    { status: 'ZERO_RESULTS',
+      message: message,
+      http_status: 200 }
+  end
   def self.invalid_request(message = "Invalid request")
     { status: 'INVALID_REQUEST',
       message: message,
       http_status: 400 }
   end
-  def self.zero_results(message = "Zero results")
-    { status: 'ZERO_RESULTS',
+  def self.over_limit(message = "Over query limit")
+    { status: 'OVER_QUERY_LIMIT',
       message: message,
-      http_status: 200 }
+      http_status: 400 }
+  end
+  # request denied can come from a bad API key, which google returns as 200, but we are sending it as 400 
+  def self.request_denied(message = "Request denied")
+    { status: 'REQUEST_DENIED',
+      message: message,
+      http_status: 400 }
+  end
+  def self.unknown_error(message = "Unknown error")
+    { status: 'UNKNOWN_ERROR',
+      message: message,
+      http_status: 400 }
   end
 end
