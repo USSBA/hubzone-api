48d55bfee8bc0ef3187fb7a5204343fe00cdbf33
Doug Price <dprice@fearless.tech>
2016-11-16 15:05:29 -0500

    rubocop on models

--- app/models/hubzone.rb
+++ app/models/hubzone.rb
@@ -3,54 +3,65 @@ class Hubzone
   def self.search(term)
     return invalid_request("Search term is blank") if term.blank?
 
-    g = Geocoder.search(term)
-    geocoder_results = JSON.parse g.body
-    case geocoder_results['status']
-    when invalid_request[:status]
-      return geocoder_results
-    when zero_results[:status]
-      return geocoder_results
-    end
+    geocoder_results = geocode term
+    return geocoder_results if (geocoder_results.eql? invalid_request[:status]) ||
+                               (geocoder_results.eql? zero_results[:status])
 
-    results = geocoder_results['results'][0]
-    results[:http_status] = g.status
-
-    location = results['geometry']['location']
-    results[:hubzone] = []
-    %w(qct brac indian_lands).each do |hz_type|
-      results[:hubzone] += assertion location, hz_type
-    end
+    results = geocode term
+    append_assertions(results)
     results
   end
 
-  def self.assertion(location, type)
-    res = ActiveRecord::Base.connection.execute(assertion_query(location, type))
-    hubzones = []
-    res.each do |r|
-      r.delete('geom')
-      r[:hz_type] = type
-      hubzones << r
-    end
-    hubzones
-  end
+  class << self
+    private
 
-  def self.assertion_query(location, type)
-    <<-SQL
+    def geocode(term)
+      g = Geocoder.search(term)
+      geocoder_results = JSON.parse g.body
+      return geocoder_results if (geocoder_results.eql? invalid_request[:status]) ||
+                                 (geocoder_results.eql? zero_results[:status])
+      results = geocoder_results['results'][0]
+      results[:http_status] = g.status
+      results
+    end
+
+    def append_assertions(results)
+      results[:hubzone] = []
+      %w(qct brac indian_lands).each do |hz_type|
+        results[:hubzone] += assertion results['geometry']['location'], hz_type
+      end
+    end
+
+    def assertion(location, type)
+      res = ActiveRecord::Base.connection.execute(assertion_query(location, type))
+      hubzones = []
+      res.each do |r|
+        r.delete('geom')
+        r[:hz_type] = type
+        hubzones << r
+      end
+      hubzones
+    end
+
+    def assertion_query(location, type)
+      <<-SQL
       SELECT *
         FROM #{type}
        WHERE ST_Intersects(geom,
          ST_GeomFromText('POINT(#{location['lng']} #{location['lat']})',4326));
-    SQL
-  end
+      SQL
+    end
 
-  def self.invalid_request(message = "Invalid request")
-    { status: 'INVALID_REQUEST',
-      message: message,
-      http_status: 400 }
-  end
-  def self.zero_results(message = "Zero results")
-    { status: 'ZERO_RESULTS',
-      message: message,
-      http_status: 200 }
+    def invalid_request(message = "Invalid request")
+      { status: 'INVALID_REQUEST',
+        message: message,
+        http_status: 400 }
+    end
+
+    def zero_results(message = "Zero results")
+      { status: 'ZERO_RESULTS',
+        message: message,
+        http_status: 200 }
+    end
   end
 end
