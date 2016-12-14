7ab3d3c8941fa43602bab1a7dcbbd1b459b89bc0
Doug Price <dprice@fearless.tech>
2016-12-14 10:33:52 -0500

    HUB-86: Adds AWS health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'search', to: 'geocode#search', as: 'search'
 
+  get 'aws-hc', to: 'health_check#status', as: 'aws_hc'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
