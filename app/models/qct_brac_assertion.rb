7343a543b1c20c70c5dafed00b63643dbf77d472
Jonathan <jon.dandois@gmail.com>
2017-03-29 09:48:20 -0400

    HUB-303 and HUB-323 syncs up API with new brac assertions and fixes conflict with latest expiration date code
    rubocop
    
    does the same for qts

--- app/models/qct_brac_assertion.rb
+++ app/models/qct_brac_assertion.rb
@@ -1,13 +1,20 @@
-# Allows for assertion of a location against the QCT_brac HubZone layer
+# Allows for assertion of a location against the qct_brac HubZone layer
 class QctBracAssertion
   extend AssertionHelper
 
   class << self
+    # rubocop:disable Metrics/AbcSize
     def assertion(location)
+      qct_combined = {'brac_id' => [], 'brac_sba_name' => [], 'expires' => '', 'hz_type' => 'qct_b'}
       assertion_by_type('qct_brac', location).each do |qct|
-        qct['hz_type'] = 'qct_b'    if qct['brac_id'].present?
-        qct
+        next unless qct['brac_id'].present?
+        qct_combined['brac_id'].push(qct['brac_id'])
+        qct_combined['brac_sba_name'].push(qct['brac_sba_name'])
+        qct_combined['county_fips'] = qct['county_fips']
+        next if qct['expires'].nil?
+        qct_combined['expires'] = qct['expires'] if qct['expires'] > qct_combined['expires']
       end
+      qct_combined['brac_id'].empty? ? [] : [qct_combined]
     end
   end
 end
--- app/models/qnmc_brac_assertion.rb
+++ app/models/qnmc_brac_assertion.rb
@@ -3,31 +3,18 @@ class QnmcBracAssertion
   extend AssertionHelper
 
   class << self
+    # rubocop:disable Metrics/AbcSize
     def assertion(location)
-      qnmc_combined = {}
-      qnmc_combined['brac_id'] = []
-      qnmc_combined['brac_sba_name'] = []
-      qnmc_combined['expires'] = []
+      qnmc_combined = {'brac_id' => [], 'brac_sba_name' => [], 'expires' => '', 'hz_type' => 'qnmc_brac'}
       assertion_by_type('qnmc_brac', location).each do |qnmc|
-        if qnmc['brac_id'].present?
-          qnmc_combined['brac_id'].push(qnmc['brac_id'])
-          qnmc_combined['hz_type'] = 'qnmc_brac'
-          qnmc_combined['brac_sba_name'].push(qnmc['brac_sba_name'])
-          qnmc_combined['county_fips'] = qnmc['county_fips']
-          unless qnmc['expires'].nil?
-            qnmc_combined['expires'].push(qnmc['expires'].to_date)
-          end
-        end
+        next unless qnmc['brac_id'].present?
+        qnmc_combined['brac_id'].push(qnmc['brac_id'])
+        qnmc_combined['brac_sba_name'].push(qnmc['brac_sba_name'])
+        qnmc_combined['county_fips'] = qnmc['county_fips']
+        next if qnmc['expires'].nil?
+        qnmc_combined['expires'] = qnmc['expires'] if qnmc['expires'] > qnmc_combined['expires']
       end
-
-      if qnmc_combined['brac_id'].empty?
-        return []
-      else
-        qnmc_combined['expires'] = qnmc_combined['expires'].max
-        qnmc_combined['brac_sba_name'] = qnmc_combined['brac_sba_name'].join('; ')
-        qnmc_combined['brac_id'] = qnmc_combined['brac_id'].join('; ')
-        return [qnmc_combined]
-      end
+      qnmc_combined['brac_id'].empty? ? [] : [qnmc_combined]
     end
   end
 end
