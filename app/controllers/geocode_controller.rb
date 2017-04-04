8f632c84933e9a7887d0376bc6aedeeb9be57543
PatKelsh <pkelsh@fearless.tech>
2017-04-04 15:16:56 -0400

    Fixed warnings from rubocop

--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -4,6 +4,6 @@ class GeocodeController < ApplicationController
     #s = params[:q]
     result = HubzoneUtil.search(q: params[:q], latlng: params[:latlng], query_date: params[:query_date])
     # Adds escape characters for single ' in the result JSON
-    render json: result.to_json.gsub("'","\\\\'"), status: result[:http_status]
+    render json: result.to_json.gsub("'", "\\\\'"), status: result[:http_status]
   end
 end
