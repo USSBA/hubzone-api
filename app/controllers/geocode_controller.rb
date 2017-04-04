7aa48f4ac31693f33104f817f75d7b677ef3ab09
PatKelsh <pkelsh@fearless.tech>
2017-04-04 15:06:20 -0400

    Added a escape characters to results JSON where needed

--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -3,6 +3,7 @@ class GeocodeController < ApplicationController
   def search
     #s = params[:q]
     result = HubzoneUtil.search(q: params[:q], latlng: params[:latlng], query_date: params[:query_date])
-    render json: result.to_json, status: result[:http_status]
+    # Adds escape characters for single ' in the result JSON
+    render json: result.to_json.gsub("'","\\\\'"), status: result[:http_status]
   end
 end
