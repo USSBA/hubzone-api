ebdc0fbd00bb9240e6e7f41a344728635ffadec9
Jonathan <jon.dandois@gmail.com>
2017-10-05 15:40:52 -0400

    HUB-877 update examples

--- app/models/likely_qda_assertion.rb
+++ app/models/likely_qda_assertion.rb
@@ -1,28 +1,41 @@
-# Allows for assertion of a location against the likely_qct_qda HubZone layer
+# Allows for assertion of a location against the likely_qda HubZone layer
 class LikelyQdaAssertion
   extend AssertionHelper
 
   class << self
     def assertion(location)
-      likely_qct_qda = [{}]
-      likely_qct_qda[0]["hz_type"] = "likely_qda"
-      # likely_qct_qda[0]["likely_qda_designations"] = []
-      # likely_qct_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qnmc_qda', location)
-      # likely_qct_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qct_qda', location)
+      likely_qda = [{}]
+      likely_qda[0]["hz_type"] = "likely_qda"
+      # likely_qda[0]["likely_qda_designations"] = []
+      # likely_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qnmc_qda', location)
+      # likely_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qct_qda', location)
 
-      likely_qct_qda[0]["likely_qda_designations"] = [
+      likely_qda[0]["likely_qda_designations"] = [
         {
           "type": "likely_qct_qda",
-          "incident_description": "Hurriane Insane",
-          "qda_declaration": "2018-02-14"
+          "incident_description": "Hurricane Irma",
+          "qda_declaration": "2017-09-15"
         },
         {
           "type": "likely_qnmc_qda",
-          "incident_description": "Hurriane Insane",
-          "qda_declaration": "2018-02-14"
+          "incident_description": "Hurricane Maria",
+          "qda_declaration": "2017-09-20"
+        },
+        {
+          "type": "likely_qnmc_qda",
+          "incident_description": "Hurricane Irma - Seminole Tribe of Florida,",
+          "qda_declaration": "2017-09-27"
+        },
+        {
+          "type": "likely_qct_qda",
+          "incident_description": "Severe Storms, Tornadoes, Straight-line Winds, and Flooding",
+          "qda_declaration": "2017-08-06"
         }
       ]
-      likely_qct_qda
+      likely_qda
     end
   end
 end
+
+
+
