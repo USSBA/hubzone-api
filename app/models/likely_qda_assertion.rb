1484e23a0f5e2cae437bd9a7955034c9da21a0d9
Jonathan <jon.dandois@gmail.com>
2017-10-05 09:39:58 -0400

    HUB-877 update mock likely qda api response as array of likely qda objects

--- app/models/likely_qda_assertion.rb
+++ app/models/likely_qda_assertion.rb
@@ -4,16 +4,24 @@ class LikelyQdaAssertion
 
   class << self
     def assertion(location)
-      # assertion_by_type('qnmc_qda', location)
       likely_qct_qda = [{}]
       likely_qct_qda[0]["hz_type"] = "likely_qda"
-      likely_qct_qda[0]["likely_qda_designations"] = {}
-      likely_qct_qda[0]["likely_qda_designations"]["type"] = "likely_qnmc_qda"
-      likely_qct_qda[0]["likely_qda_designations"]["incident_description"] = "Hurriane Insane"
-      likely_qct_qda[0]["likely_qda_designations"]["QDA Declaration"] = "2018-02-14"
-      likely_qct_qda[0]["likely_qda_designations"]["type"] = "likely_qct_qda"
-      likely_qct_qda[0]["likely_qda_designations"]["incident_description"] = "Hurriane Insane"
-      likely_qct_qda[0]["likely_qda_designations"]["QDA Declaration"] = "2018-02-14"
+      # likely_qct_qda[0]["likely_qda_designations"] = []
+      # likely_qct_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qnmc_qda', location)
+      # likely_qct_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qct_qda', location)
+
+      likely_qct_qda[0]["likely_qda_designations"] = [
+        {
+          "type": "likely_qct_qda",
+          "incident_description": "Hurriane Insane",
+          "qda_declaration": "2018-02-14"
+        },
+        {
+          "type": "likely_qnmc_qda",
+          "incident_description": "Hurriane Insane",
+          "qda_declaration": "2018-02-14"
+        }
+      ]
       likely_qct_qda
     end
   end
