9041fac33bd522ffa01d7286653027e88f2f5a20
Jonathan <jon.dandois@gmail.com>
2017-10-09 14:38:37 -0400

    HUB-877 updates api to call likely_qda from db and pass through

--- app/models/likely_qda_assertion.rb
+++ app/models/likely_qda_assertion.rb
@@ -4,38 +4,14 @@ class LikelyQdaAssertion
 
   class << self
     def assertion(location)
-      likely_qda = [{}]
-      likely_qda[0]["hz_type"] = "likely_qda"
-      # likely_qda[0]["likely_qda_designations"] = []
-      # likely_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qnmc_qda', location)
-      # likely_qda[0]["likely_qda_designations"].push assertion_by_type('likely_qct_qda', location)
-
-      likely_qda[0]["likely_qda_designations"] = [
-        {
-          "type": "likely_qct_qda",
-          "incident_description": "Hurricane Irma",
-          "qda_declaration": "2017-09-15"
-        },
-        {
-          "type": "likely_qnmc_qda",
-          "incident_description": "Hurricane Maria",
-          "qda_declaration": "2017-09-20"
-        },
-        {
-          "type": "likely_qnmc_qda",
-          "incident_description": "Hurricane Irma - Seminole Tribe of Florida,",
-          "qda_declaration": "2017-09-27"
-        },
-        {
-          "type": "likely_qct_qda",
-          "incident_description": "Severe Storms, Tornadoes, Straight-line Winds, and Flooding",
-          "qda_declaration": "2017-08-06"
-        }
-      ]
-      likely_qda
+      likely_qdas = []
+      assertion_by_type('likely_qda', location).each do |qda|
+        likely_qda = {}
+        likely_qda["incident_description"] = qda["incident_description"]
+        likely_qda["qda_declaration"] = qda["declaration_date"]
+        likely_qdas.push(likely_qda)
+      end
+      likely_qdas
     end
   end
 end
-
-
-
