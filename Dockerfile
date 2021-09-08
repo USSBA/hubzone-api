39c4db6d5822544d4934df6f72c1d699f1cc9279
Charles Bushong <cbushong@fearless.tech>
2021-09-08 08:32:29 -0400

    IA-2139: Update dependencies for docker rebuild

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
 FROM ruby:2.5-slim-stretch
 
 # Install general packages
-ENV PACKAGES build-essential libpq-dev netcat git python3 python-pip python-dev apt-utils wget unzip lftp ssh jq gnupg
+ENV PACKAGES build-essential libpq-dev netcat git apt-utils wget unzip lftp ssh jq gnupg
 RUN echo "Updating repos..." && apt-get update > /dev/null && \
     echo "Installing packages: ${PACKAGES}..." && apt-get install -y $PACKAGES --fix-missing --no-install-recommends > /dev/null && \
     echo "Done" && rm -rf /var/lib/apt/lists/*
 
-# Install aws-cli
-RUN echo "Fetching awscli installer..." && wget -qO "awscli-bundle.zip" "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" && \
-    echo "Unpacking..." && unzip awscli-bundle.zip > /dev/null && \
-    echo "Installing awscli..." && ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws > /dev/null && \
-    echo "Done" && rm -rf awscli-bundle awscli-bundle.zip
-
 # Configure/Install Postgres Repos/Deps
-ENV PG_PACKAGES postgresql-9.6 postgresql-9.6-postgis-2.4
+ENV PG_PACKAGES postgresql-client
 RUN echo deb http://apt.postgresql.org/pub/repos/apt stretch-pgdg main > /etc/apt/sources.list.d/stretch-pgdg.list && \
     wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
 RUN echo "Updating repos..." && apt-get update > /dev/null && \
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,73 +3,28 @@
 export AWS_DEFAULT_REGION=us-east-1
 export SERVICE_NAME=hubzone-api
 
-function local-usage () {
-  echo "FATAL: Must set all environment variables before this container can be launched. Exiting.
-
-  Usage: Container must be run with the following environment variables:
-  SECRET_KEY_BASE
-  HUBZONE_API_DB_PASSWORD
-  HUBZONE_GOOGLE_API_KEY
-"
-}
-
-function aws-usage () {
-  echo "FATAL: Must set all environment variables before this container can be launched. Exiting.
-
-  Usage: Container must be run with the following environment variables:
-  AWS_ENVIRONMENT: Resource environment for this service.  Used to reference ParameterStore values.  Ex: dev, demo, qa, stg, prod, trn
-"
-}
-
-function getparameterstore () {
-  ENV_NAME=$1
-  PARAMETER_STORE_NAME=$2
-  echo "Getting parameter ENV_NAME=${ENV_NAME}, PARAMETER_STORE_NAME=${PARAMETER_STORE_NAME}"
-  echo "Testing pulling data from ParameterStore..."
-  aws ssm get-parameter --name "${PARAMETER_STORE_NAME}" > /dev/null && echo "Success ${PARAMETER_STORE_NAME}" || ( echo "FATAL: Could not retrieve ParameterStore value '${PARAMETER_STORE_NAME}'"; exit 10; )
-  export $ENV_NAME=$(aws ssm get-parameter --name "${PARAMETER_STORE_NAME}" | jq .Parameter.Value -r)
-
-  if [ -z "${!ENV_NAME}" ]; then
-    echo "FATAL: Could not retrieve ParameterStore value '${PARAMETER_STORE_NAME}'"
-    exit 15
-  else
-    echo "Successfully loaded ${ENV_NAME}"
+function check_param() {
+  if [ -z "${!1}" ]; then
+    echo "FATAL ERROR, CONTAINER WILL EXIT:  missing required Environment Variable '$1'"
+    MISSING_PARAM=1
   fi
 }
-
-function get_dotenv_s3 () {
-  echo "Beginning sync of s3://${DOTENV_S3_PATH}..."
-  aws s3 cp "s3://${DOTENV_S3_PATH}" . --recursive || ( echo "FATAL: Could not sync files from S3"; exit 30; )
-  echo "Completed S3 File sync."
+function wait_for {
+  echo -n "Checking network connectivity for: ${1}:${2}..."
+  while ! nc -w5 -z "${1}" "${2}"; do
+    echo -n "."
+    sleep 1
+  done
+  echo " Connected."
 }
 
-# hubzone-api Specific Configuration
-
-# Check if running in AWS
-if aws sts get-caller-identity > /dev/null 2>&1; then
-  if [ -z "${AWS_ENVIRONMENT}" ]; then
-    aws-usage
-    exit 35
-  else
-    # We're using aws, pull some env vars from parameter store
-    echo "Entrypoint: running in AWS"
-    ## Get Parameters from Parameter Store
-    getparameterstore SECRET_KEY_BASE "${AWS_ENVIRONMENT}-${SERVICE_NAME}-secret_key_base"
-    getparameterstore HUBZONE_API_DB_PASSWORD "${AWS_ENVIRONMENT}-hubzone-db_password"
-    getparameterstore HUBZONE_GOOGLE_API_KEY "${AWS_ENVIRONMENT}-hubzone-google_api_key"
-
-    ## Pull other configuration files from s3
-    #get_dotenv_s3
-  fi
-else
-  echo "Entrypoint: NOT running in AWS"
-  if [ -z "${SECRET_KEY_BASE}" ] ||
-     [ -z "${HUBZONE_API_DB_PASSWORD}" ]
-     [ -z "${HUBZONE_GOOGLE_API_KEY}" ]
-     then
-    local-usage
-    exit 40
-  fi
-fi
+MISSING_PARAM=0
+check_param SECRET_KEY_BASE
+check_param HUBZONE_API_DB_HOST
+check_param HUBZONE_API_DB_USER
+check_param HUBZONE_API_DB_NAME
+check_param HUBZONE_API_DB_PASSWORD
+check_param HUBZONE_GOOGLE_API_KEY
+if [ "$MISSING_PARAM" == "1" ]; then exit 1; fi
 
 exec "$@"
