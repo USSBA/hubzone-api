b989ecad1635fd16ffba51a4ac033976188f0b10
Jason Smigovsky <jsmigovsky@fearless.tech>
2021-09-14 11:49:40 -0400

    IA-2139: fix rubocop findings

--- terraform/security-group-rules.tf
+++ terraform/security-group-rules.tf
@@ -1,8 +1,8 @@
 # messaging to RDS
 resource "aws_security_group_rule" "api_to_rds" {
   type                     = "ingress"
-  from_port                = 0
-  to_port                  = 65535
+  from_port                = 5432
+  to_port                  = 5432
   protocol                 = "tcp"
   source_security_group_id = module.api.security_group_id
   security_group_id        = data.aws_db_instance.rds.vpc_security_groups[0]
