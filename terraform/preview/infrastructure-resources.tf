2a1f3abc578ecef28c7797fe8308a944afd38cbc
Ryan Whorton <rwhorton@oasysic.com>
2023-07-11 14:43:21 -0400

    adding archive tf

---
+++ terraform/preview/infrastructure-resources.tf
@@ -0,0 +1,65 @@
+# vpc
+data "aws_vpc" "selected" {
+  tags = {
+    Name = "${terraform.workspace}-vpc"
+  }
+}
+
+# subnet ids
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name = "tag:Name"
+    values = [
+      "${terraform.workspace}-private-subnet-*"
+    ]
+  }
+}
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name = "tag:Name"
+    values = [
+      "${terraform.workspace}-public-subnet-*"
+    ]
+  }
+}
+
+## hosted zone
+data "aws_route53_zone" "selected" {
+  name = "${local.env.fqdn_base}."
+}
+
+## acm
+data "aws_acm_certificate" "selected" {
+  domain      = local.env.cert_domain
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
+## ecs cluster
+data "aws_ecs_cluster" "selected" {
+  cluster_name = terraform.workspace
+}
+
+## Elasticache Redis
+#data "aws_elasticache_replication_group" "redis" {
+#  replication_group_id = "${terraform.workspace}-shared-services"
+#}
+#data "aws_elasticache_cluster" "redis" {
+#  cluster_id = tolist(data.aws_elasticache_replication_group.redis.member_clusters)[0]
+#}
+
+## RDS Postgres Instance
+data "aws_db_instance" "rds" {
+  db_instance_identifier = "${terraform.workspace}-preview-hubzone"
+}
+#data "aws_rds_cluster" "rds" {
+#  cluster_identifier = "${terraform.workspace}-hubzone"
+#}
---
+++ terraform/preview/locals.tf
@@ -0,0 +1,73 @@
+locals {
+  region       = data.aws_region.current.name
+  account_id   = data.aws_caller_identity.current.account_id
+  account_name = contains(["stg", "prod"], terraform.workspace) ? "upper" : "lower"
+  account_ids = {
+    demo = "997577316207"
+    stg  = "222484291001"
+    prod = "222484291001"
+  }
+  all = {
+    default = {
+      service_name     = "preview-hubzone-api"
+      ecr_name         = "hubzone-api-preview"
+      public_subdomain = "preview-maps"
+      geo_db_name      = "hzgeo_${terraform.workspace}"
+
+      rails_port        = 3001
+      task_cpu_rails    = "256"
+      task_memory_rails = "512"
+      log_bucket        = "${local.account_id}-logs"
+
+      health_check_path             = "/api/aws-hc"
+      desired_container_count_rails = 1 # the starting number of containers
+      max_container_count_rails     = 1 # maximum number of containers
+      min_container_count_rails     = 1 # minimum number of containers
+      scaling_metric                = "memory"
+      scaling_threshold             = "75"
+
+
+      scheduled_actions          = []
+      scheduled_actions_timezone = "America/New_York"
+    }
+    demo = {
+      fqdn_base        = "demo.sba-one.net"
+      cert_domain      = "sba-one.net"
+      public_subdomain = "hubzone"
+      rails_env        = "demo"
+    }
+    stg = {
+      fqdn_base   = "stg.certify.sba.gov"
+      cert_domain = "stg.certify.sba.gov"
+      rails_env   = "staging"
+
+      desired_container_count_rails = 2
+      min_container_count_rails     = 2
+      max_container_count_rails     = 2
+
+      scheduled_actions = [
+        { expression = "cron(0 7 * * ? *)", max_capacity = 2, min_capacity = 2 },  # Everyday at 7:00 AM EST
+        { expression = "cron(0 19 * * ? *)", max_capacity = 1, min_capacity = 1 }, # Everyday at 7:00 PM EST
+      ]
+    }
+    prod = {
+      fqdn_base                     = "certify.sba.gov"
+      cert_domain                   = "certify.sba.gov"
+      rails_env                     = "production"
+      desired_container_count_rails = 2
+      min_container_count_rails     = 2
+      max_container_count_rails     = 4
+    }
+  }
+  # Condense all config into a single `local.env.*`
+  env = merge(local.all.default, try(local.all[terraform.workspace], {}))
+
+  service_fqdn  = "${local.env.service_name}.${local.env.fqdn_base}"
+  public_fqdn   = "preview-maps.${local.env.fqdn_base}"
+  postgres_fqdn = "preview-hubzone-db.${local.env.fqdn_base}"
+
+  # Convenience prefixes for AWS Resources
+  prefix_bucket          = "arn:aws:s3:::"
+  prefix_ecr             = "${local.account_id}.dkr.ecr.${local.region}.amazonaws.com"
+  prefix_parameter_store = "arn:aws:ssm:${local.region}:${local.account_id}:parameter"
+}
